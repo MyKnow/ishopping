e278afbf1a175baa8698892a09518fa9465b0e1b
MyKnow <myknow000@gmail.com>
2023-11-16 21:40:56 +0900

    NativeView 주석 처리

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,17 +1,27 @@
 import Flutter
 import UIKit
 
-@UIApplicationMain
+@UIApplicationMain // 어노테이션으로 이 클래스가 iOS 애플리케이션의 진입점임을 나타냄.
 @objc class AppDelegate: FlutterAppDelegate {
+    // 애플리케이션이 시작된 후 추가 설정을 수행하기 위해 이 메소드를 오버라이드
+
+    // 현재 실행 중인 앱
+    // 앱이 시작될 때 전달되는 옵션을 포함하는 딕셔너리.
+    // 앱이 시작된 이유와 관련된 다양한 키-값 쌍 포함 (알림을 통해 시작, URL을 통해 시작... etc)
     override func application(
       _ application: UIApplication,
       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?
     ) -> Bool {
+      // Flutter 플러그인을 등록
       GeneratedPluginRegistrant.register(with: self)
 
+      // "plugin-name"이라는 플러그인의 레지스트라(registrar)에 대한 약한 참조를 얻음. (plugin-name은 플레이스홀더 이름)
       weak var registrar = self.registrar(forPlugin: "plugin-name")
 
+      // 인스턴스를 생성하고, 레지스트라로부터 얻은 메신저와 함께 초기화
       let factory = FLNativeViewFactory(messenger: registrar!.messenger())
+
+      // 이 팩토리를 플러그인 레지스트라에 특정 플러그인 이름과 뷰 타입 식별자와 함께 등록
       self.registrar(forPlugin: "<plugin-name>")!.register(
         factory,
         withId: "<platform-view-type>")
--- ios/Runner/FLNativeView.swift
+++ ios/Runner/FLNativeView.swift
@@ -1,16 +1,21 @@
 import Flutter
 import UIKit
 
+// Fluter에서 사용자 정의 플랫폼 뷰를 생성하는 데 필요함
 class FLNativeViewFactory: NSObject, FlutterPlatformViewFactory {
+    // Flutter와 Native 코드 간의 통신을 위한 Flutter 바이너리 메신저에 대한 참조를 저장함
     private var messenger: FlutterBinaryMessenger
 
+    // FlutterBinaryMessenger로 팩토리?를 초기화함
     init(messenger: FlutterBinaryMessenger) {
         self.messenger = messenger
         super.init()
     }
 
+    // FLNativeView를 생성하고 반환함
+    // 뷰의 프레임, 식별자, 선택적 인자
     func create(
-        withFrame frame: CGRect,
+        withFrame frame: CGRect, 
         viewIdentifier viewId: Int64,
         arguments args: Any?
     ) -> FlutterPlatformView {
@@ -21,15 +26,19 @@ class FLNativeViewFactory: NSObject, FlutterPlatformViewFactory {
             binaryMessenger: messenger)
     }
 
-    /// Implementing this method is only necessary when the `arguments` in `createWithFrame` is not `nil`.
+    // Flutter와 네이티브 코드 간의 메시지를 인코딩 및 디코딩하기 위한 메시지 코덱을 반환함
+    // create 메소드에서 비-nil 인자를 사용할 경우에만 필요
     public func createArgsCodec() -> FlutterMessageCodec & NSObjectProtocol {
           return FlutterStandardMessageCodec.sharedInstance()
     }
 }
 
+// FlutterPlatformView 프로토콜을 구현하여 Flutter 뷰로 사용될 수 있음
 class FLNativeView: NSObject, FlutterPlatformView {
+    // 기본적인 네이티브 iOS 뷰
     private var _view: UIView
 
+    // 뷰의 프레임, 뷰 식별자, 선택적 인자, 그리고 바이너리 메신저를 사용하여 네이티브 뷰를 초기화
     init(
         frame: CGRect,
         viewIdentifier viewId: Int64,
@@ -38,16 +47,18 @@ class FLNativeView: NSObject, FlutterPlatformView {
     ) {
         _view = UIView()
         super.init()
-        // iOS views can be created here
+        // createNativeView를 호출하여 네이티브 뷰를 설정
         createNativeView(view: _view)
     }
 
+    // 기본 UIView 객체를 반환
     func view() -> UIView {
         return _view
     }
 
+    // 네이티브 뷰(_view)를 설정
     func createNativeView(view _view: UIView){
-        _view.backgroundColor = UIColor.blue
+        _view.backgroundColor = UIColor.black
         let nativeLabel = UILabel()
         nativeLabel.text = "Native text from iOS"
         nativeLabel.textColor = UIColor.white
