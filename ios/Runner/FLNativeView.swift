7bb686e62b17b38d08d98e20f030919cdcc1544f
MyKnow <myknow000@gmail.com>
2023-11-22 22:23:48 +0900

    오버레이 꽉 채운 화면

--- ios/Runner/FLNativeView.swift
+++ ios/Runner/FLNativeView.swift
@@ -155,16 +155,6 @@ class FLNativeView: NSObject, FlutterPlatformView, ARSCNViewDelegate {
         }
     }
 
-    // 
-    private func convertDepthDataToUIImage(_ depthMap: CVPixelBuffer) -> UIImage? {
-        let ciImage = CIImage(cvPixelBuffer: depthMap)
-        let context = CIContext(options: nil)
-        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else {
-            return nil
-        }
-        return UIImage(cgImage: cgImage, scale: 1.0, orientation: .right)
-    }
-
     private func processFrame(_ frame: ARFrame) {
         let pixelBuffer = frame.capturedImage
         // pixelBuffer에서 고해상도 이미지를 생성 및 처리
@@ -183,7 +173,6 @@ class FLNativeView: NSObject, FlutterPlatformView, ARSCNViewDelegate {
         // 이미지 방향 설정 (반시계로 돌아가는 문제)
         return UIImage(cgImage: cgImage, scale: 1.0, orientation: .right)
     }
-
 
     private func saveImageToPhotoLibrary(_ image: UIImage) {
         ////// 서버로 전송 시도 /////
@@ -296,11 +285,13 @@ class FLNativeView: NSObject, FlutterPlatformView, ARSCNViewDelegate {
     // Depth map 오버레이를 추가하는 메서드
     private func overlayDepthMap(_ depthMap: CVPixelBuffer) {
         guard let depthImage = convertDepthDataToUIImage(depthMap) else { return }
+        dump(depthMap)
 
         if depthOverlayView == nil {
             depthOverlayView = UIImageView(frame: arView.bounds)
-            depthOverlayView?.contentMode = .scaleAspectFit
-            depthOverlayView?.alpha = 0.5 // 반투명하게 설정
+            depthOverlayView?.contentMode = .scaleAspectFill // 변경: 이미지가 뷰의 경계를 채우도록 설정
+            depthOverlayView?.clipsToBounds = true // 뷰 경계 밖의 이미지 부분을 잘라냄
+            depthOverlayView?.alpha = 0.8 // 반투명 설정
             arView.addSubview(depthOverlayView!)
         }
 
@@ -308,6 +299,16 @@ class FLNativeView: NSObject, FlutterPlatformView, ARSCNViewDelegate {
         depthOverlayView?.image = depthImage
     }
 
+    //
+    private func convertDepthDataToUIImage(_ depthMap: CVPixelBuffer) -> UIImage? {
+        let ciImage = CIImage(cvPixelBuffer: depthMap)
+        let context = CIContext(options: nil)
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else {
+            return nil
+        }
+        return UIImage(cgImage: cgImage, scale: 1.0, orientation: .right)
+        //return UIImage(cgImage: cgImage, scale: 1.0, orientation: .up)
+    }
 
     private func updateGridDotsPosition() {
         let screenWidth = arView.bounds.width
@@ -365,9 +366,6 @@ class FLNativeView: NSObject, FlutterPlatformView, ARSCNViewDelegate {
         // 매핑된 색상이 없는 경우 기본 색상 반환
         return .white
     }
-
-
-
 
     // 기본 UIView 객체를 반환
     func view() -> UIView {
